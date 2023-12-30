dd22ea17662b6ad95172ce2c45f454cada9c60dc
aftxrlifx <96382604+its-Jaxx@users.noreply.github.com>
2023-12-30 15:33:47 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const imageDir = path.join(__dirname, "Images"); 
+
+app.get("/", (req, res) => {
+  res.send("Huge thanks to nismo#1337 for helping me out with all the cloud stuff (Hella confusing)");
+});
+
+app.get("/meow.png", (req, res) => {
+  fs.readdir(imageDir, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return res.status(500).send("Error reading directory");
+    }
+
+    const imageFiles = files.filter(file => {
+      return file.toLowerCase().endsWith(".jpg") || file.toLowerCase().endsWith(".png");
+    });
+
+    if (imageFiles.length === 0) {
+      return res.status(404).send("No images found");
+    }
+
+    const randomIndex = Math.floor(Math.random() * imageFiles.length);
+    const randomImage = path.join(imageDir, imageFiles[randomIndex]);
+
+    res.sendFile(randomImage); 
+  });
+});
+
+const port = process.env.PORT || 3000; 
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
